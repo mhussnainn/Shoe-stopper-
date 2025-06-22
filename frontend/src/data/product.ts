acc7473bbdd5a40b3e96221a4940e92d70960c56
mhussnainn <hussnain.anwar19@gmail.com>
2025-06-22 19:59:27 +0500

    Products updated

--- frontend/src/data/product.ts
+++ frontend/src/data/product.ts
@@ -18,10 +18,10 @@ export type Product = {
 export const products: Product[] = [
 { id: '1', name: 'BMW AIR SNEAKER', price: 15000, image: BMW, category: 'men' },
 { id: '2', name: 'UMBREON POKEMON AIR SNEAKER', price: 15000, image: Black_Pika, category: 'men' },
-{ id: '3', name: 'TOM & JERRY AIR SNEAKER', price: 15000, image: Destop, category: 'men' },
+{ id: '3', name: 'TOM & JERRY AIR SNEAKER', price: 15000, image: Destop, category: 'women' },
 { id: '4', name: 'KINGDRA POKEMON AIR SNEAKER', price: 15000, image: Dina, category: 'women' },
 { id: '5', name: 'CLEFAIRY POKEMON AIR SNEAKER', price: 15000, image: FePika, category: 'women' },
-{ id: '6', name: 'DREDNAW POKEMON AIR SNEAKER', price: 15000, image: Ninja, category: 'women' },
+{ id: '6', name: 'DREDNAW POKEMON AIR SNEAKER', price: 15000, image: Ninja, category: 'men' },
 { id: '7', name: 'CORPHISH POKEMON AIR FORCE', price: 15000, image: Pika, category: 'men' },
 { id: '8', name: 'BULBASAUR POKEMON AIR SNEAKER', price: 15000, image: Tortoise, category: 'women' },
 ];
